cea6467314f8828108d9c6ee03d4d961ffebfa3b
Saksham Saini <sakshamsaini18@gmail.com>
2025-01-28 07:49:17 -0500

    fix product hunt badge for mobile

--- src/lib/components/ProductHunt.svelte
+++ src/lib/components/ProductHunt.svelte
@@ -1,11 +1,13 @@
-<a
-  href="https://www.producthunt.com/posts/devleague?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-devleague"
-  target="_blank"
-  ><img
-    src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=824160&theme=light&t=1738066837488"
-    alt="DevLeague - Your&#0032;Github&#0032;profile&#0032;as&#0032;a&#0032;Pokemon&#0032;card | Product Hunt"
-    style="width: 250px; height: 54px;"
-    width="250"
-    height="54"
-  /></a
->
+<div class="self-center md:self-start mb-6">
+  <a
+    href="https://www.producthunt.com/posts/devleague?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-devleague"
+    target="_blank"
+    ><img
+      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=824160&theme=light&t=1738066837488"
+      alt="DevLeague - Your&#0032;Github&#0032;profile&#0032;as&#0032;a&#0032;Pokemon&#0032;card | Product Hunt"
+      style="width: 250px; height: 54px;"
+      width="250"
+      height="54"
+    /></a
+  >
+</div>
